feb5441700eaa2b0beea7e42a825e1e0106f0378
Naomi Villanueva <naovillaj@gmail.com>
2017-04-06 22:34:01 -0500

    menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,15 @@ document.getElementById('toggle').addEventListener("click", function(event){
 	event.preventDefault();
 	document.getElementById("nav-header").classList.toggle("open");
 	document.getElementById("body").classList.toggle("overflow-hidden");
+});
+
+var lastScrollTop = 0;
+window.addEventListener("scroll", function(){
+	var currentScroll = window.pageYOffset || document.body.scrollTop;
+	if(currentScroll > lastScrollTop){
+		console.log("down");
+	}else{
+		console.log("up");
+	}
+	lastScrollTop = currentScroll;
 })
